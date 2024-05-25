130eaf3464096e9ed44d6c4742d3a4f14dd0798e
amitvarma52 <amitv2529@gmail.com>
2024-05-25 08:54:23 +0530

    mouseOver on cards added

--- src/Components/CardsChild/CardsChild.jsx
+++ src/Components/CardsChild/CardsChild.jsx
@@ -2,15 +2,23 @@ import React, { useRef, useState } from "react";
 import style from "./CardsChild.module.css";
 import TrendingChild from "../Trending/TrendingChild/TrendingChild.jsx";
 const CardsChild = ({ title, cardContainer }) => {
+  const handleOn=(e)=>{
+    e.currentTarget.children[1].style.display="block"
+  }
+  const handleOff=(e)=>{
+    e.currentTarget.children[1].style.display="none"
+  }
   return (
     <>
       <h3>{title}</h3>
       <div className={style.container}>
         {cardContainer.map((element) => {
           return (
-            <div className={style.card} key={element.id}>
+            <div className={style.card} key={element.id} onMouseOver={handleOn} onMouseLeave={handleOff}>
               <img src={element.imageUrl}></img>
+              <div className={style.show}>
               <TrendingChild p={element.description} />
+              </div>
             </div>
           );
         })}
